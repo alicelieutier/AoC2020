d65e22613f8a713f1e5dc31cce6bb105315becd3
Alice Lieutier <alice.lieutier@gmail.com>
2020-12-12 13:16:07 +0000

    refactor

--- day12/day12.py
+++ day12/day12.py
@@ -12,40 +12,42 @@ def parse_instruction(line):
     action, value = match.group('action'), int(match.group('value'))
     return (action, value)
 
-RIGHT_TURN = ['N', 'E', 'S', 'W']
-def calculate_new_facing_direction(initial_direction, action, value):
-    if action not in {'L', 'R'}:
-        return initial_direction
-    angle = value if action == 'R' else 360 - value
-    offset = RIGHT_TURN.index(initial_direction)
-    quarter_turns = angle // 90
-    return RIGHT_TURN[(offset + quarter_turns) % 4]
-
-NS_EW_VALUES = {
-    'N': lambda ns, ew, value : (ns + value, ew),
-    'S': lambda ns, ew, value : (ns - value, ew),
-    'E': lambda ns, ew, value : (ns, ew + value),
-    'W': lambda ns, ew, value : (ns, ew - value),
-    'L': lambda ns, ew, _ : (ns, ew),
-    'R': lambda ns, ew, _ : (ns, ew),
+ABSOLUTE_MOVE = {
+    'N': lambda x, y, value : (x, y + value),
+    'S': lambda x, y, value : (x, y - value),
+    'E': lambda x, y, value : (x + value, y),
+    'W': lambda x, y, value : (x - value, y),
 }
-def run_intructions(instructions):
-    def aux(acc, instruction):
-        ns, ew, facing = acc
-        action, value = instruction
-        if action == 'F':
-            action = facing
-        ns, ew = NS_EW_VALUES[action](ns, ew, value)
-        return ns, ew, calculate_new_facing_direction(facing, action, value)
-    return reduce(aux, instructions, (0, 0, 'E'))
 
+LEFT_TURN = ['N', 'W', 'S', 'E']
+def calculate_new_facing_direction(initial_direction, action, value):
+    angle = value if action == 'L' else 360 - value
+    offset = LEFT_TURN.index(initial_direction)
+    quarter_turns = angle // 90
+    return LEFT_TURN[(offset + quarter_turns) % 4]
+
+def run_part1_intructions(instructions):
+    def aux(acc, instruction):
+        x, y, facing = acc
+        action, value = instruction
+        if action in {'L', 'R'}:
+            facing = calculate_new_facing_direction(facing, action, value)
+        elif action == 'F':
+            x, y = ABSOLUTE_MOVE[facing](x, y, value)
+        else:
+            x, y = ABSOLUTE_MOVE[action](x, y, value)
+        return x, y, facing
+    return reduce(aux, instructions, (0, 0, 'E'))
 
 def simple_sine(angle): return [0, 1, 0, -1][angle // 90 % 4]
 def simple_cosine(angle): return [1, 0, -1, 0][angle // 90 % 4]
 
+"""
+From https://math.stackexchange.com/questions/270194/how-to-find-the-vertices-angle-after-rotation
+  𝑥′=(𝑥−𝑝)cos(𝜃)−(𝑦−𝑞)sin(𝜃)+𝑝
+  𝑦′=(𝑥−𝑝)sin(𝜃)+(𝑦−𝑞)cos(𝜃)+𝑞
+"""
 def coords_after_rotation(ship_coords, waypoint_coords, action, value):
-    # 𝑥′=(𝑥−𝑝)cos(𝜃)−(𝑦−𝑞)sin(𝜃)+𝑝
-    # 𝑦′=(𝑥−𝑝)sin(𝜃)+(𝑦−𝑞)cos(𝜃)+𝑞
     p, q = ship_coords
     x, y = waypoint_coords
     theta = value if action == 'L' else 360 - value
@@ -59,16 +61,9 @@ def coords_after_move_forward(ship_coords, waypoint_coords, value):
     move_x, move_y = value * (wx - sx), value * (wy - sy)
     return (sx + move_x, sy + move_y), (wx + move_x, wy + move_y)
 
-ABSOLUTE_MOVE = {
-    'N': lambda x, y, value : (x, y + value),
-    'S': lambda x, y, value : (x, y - value),
-    'E': lambda x, y, value : (x + value, y),
-    'W': lambda x, y, value : (x - value, y),
-}
 def coords_after_waypoint_move(ship_coords, waypoint_coords, action, value):
     wx, wy = waypoint_coords
-    new_x, new_y = ABSOLUTE_MOVE[action](wx, wy, value)
-    return ship_coords, (new_x, new_y)
+    return ship_coords, ABSOLUTE_MOVE[action](wx, wy, value)
 
 def run_part2_instructions(instructions):
     def aux(acc, instruction):
@@ -86,8 +81,8 @@ file = './day12/input'
 instructions = parse_instructions(file)
 
 # Part 1
-final_ns, final_ew, _ = run_intructions(instructions)
-print(abs(final_ns) + abs(final_ew))
+x, y, _ = run_part1_intructions(instructions)
+print(abs(x) + abs(y))
 
 # Part 2
 (x, y), _ = run_part2_instructions(instructions)
@@ -100,8 +95,6 @@ assert(calculate_new_facing_direction('N', 'R', 180) == 'S')
 assert(calculate_new_facing_direction('S', 'R', 270) == 'E')
 assert(calculate_new_facing_direction('E', 'L', 180) == 'W')
 assert(calculate_new_facing_direction('W', 'L', 90) == 'S')
-assert(calculate_new_facing_direction('E', 'N', 23) == 'E')
-assert(calculate_new_facing_direction('W', 'E', 6) == 'W')
 
 # Part 2
 assert(simple_sine(270) == -1)
