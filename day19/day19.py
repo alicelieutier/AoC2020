798165de38f0c0c49ece739171186abc3c253477
Alice Lieutier <alice.lieutier@gmail.com>
2020-12-19 19:43:38 +0000

    comments and naming

--- day19/day19.py
+++ day19/day19.py
@@ -22,18 +22,25 @@ def parse_rule(line):
 
 def memoized_skip_first(f):
     memo = {}
+    # magic number based on trial error
+    # lower means faster
+    # fastest that was still accurate was 8 for my input.
     NB_OF_CALLS = 8
     def helper(skip, x):
         if x not in memo:
+            # Storing the result from first call
+            # which means part 2 rules will default to part 1
+            # rules after NB_OF_CALLS calls.
             memo[x] = [f(skip, x), 1]
         memo[x][1] += 1
         if memo[x][1] > NB_OF_CALLS:
+            # returning cached part 1 result
             return memo[x][0]
         return f(skip, x)
     return helper
 
 @memoized_skip_first
-def expand_rules(rules, id):
+def build_pattern(rules, id):
     rule = rules[id]
     if rule['type'] == 'letter':
         return rule['letter']
@@ -42,7 +49,7 @@ def expand_rules(rules, id):
         for branch in rule['rules']:
             branch_letters = []
             for rule_id in branch:
-                branch_letters.append(expand_rules(rules, rule_id))
+                branch_letters.append(build_pattern(rules, rule_id))
             possibles.append(''.join(branch_letters))
         return '(' +'|'.join(possibles) + ')'
 
@@ -56,12 +63,12 @@ file = './day19/input'
 # Part 1
 rules, words = parse_file(file)
 rules_dict = {k: v for k, v in map(parse_rule, rules)}
-pattern = re.compile(expand_rules(rules_dict, '0'))
+pattern = re.compile(build_pattern(rules_dict, '0'))
 print(len([word for word in words if check_word(pattern, word)]))
 
 # Part 2
 # add this to rules - now with loops!
 extra_rules = ['8: 42 | 42 8', '11: 42 31 | 42 11 31']
 p2_rules_dict = {k: v for k, v in map(parse_rule, rules + extra_rules)}
-p2_pattern = re.compile(expand_rules(p2_rules_dict, '0'))
+p2_pattern = re.compile(build_pattern(p2_rules_dict, '0'))
 print(len([word for word in words if check_word(p2_pattern, word)]))
