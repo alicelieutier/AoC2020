925d6404167b97ad4abdfe1155704c89f29ec2a0
Alice Lieutier <alice.lieutier@gmail.com>
2020-12-19 19:36:07 +0000

    day19 - part2

--- day19/day19.py
+++ day19/day19.py
@@ -22,11 +22,15 @@ def parse_rule(line):
 
 def memoized_skip_first(f):
     memo = {}
+    NB_OF_CALLS = 8
     def helper(skip, x):
         if x not in memo:
-            memo[x] = f(skip, x)
-        return memo[x]
-    return helper 
+            memo[x] = [f(skip, x), 1]
+        memo[x][1] += 1
+        if memo[x][1] > NB_OF_CALLS:
+            return memo[x][0]
+        return f(skip, x)
+    return helper
 
 @memoized_skip_first
 def expand_rules(rules, id):
@@ -54,3 +58,10 @@ rules, words = parse_file(file)
 rules_dict = {k: v for k, v in map(parse_rule, rules)}
 pattern = re.compile(expand_rules(rules_dict, '0'))
 print(len([word for word in words if check_word(pattern, word)]))
+
+# Part 2
+# add this to rules - now with loops!
+extra_rules = ['8: 42 | 42 8', '11: 42 31 | 42 11 31']
+p2_rules_dict = {k: v for k, v in map(parse_rule, rules + extra_rules)}
+p2_pattern = re.compile(expand_rules(p2_rules_dict, '0'))
+print(len([word for word in words if check_word(p2_pattern, word)]))
